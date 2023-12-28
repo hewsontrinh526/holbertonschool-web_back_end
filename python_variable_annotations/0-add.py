8a2f46293891dc5cff00b53bdcb66c8a3901c6df
hewsontrinh526 <6734@holbertonstudents.com>
2023-12-28 16:12:49 +1100

    Feat: A type-annotated function add that takes a float a and b as arguments

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+A type-annotated function add that takes a float a and a float b as arguments
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of a and b
+    """
+    return a + b
