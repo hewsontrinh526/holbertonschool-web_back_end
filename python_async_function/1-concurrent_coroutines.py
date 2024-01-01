1bfe55b2bea18fc9a34f51fa14e0ce2d426c04e3
hewsontrinh526 <6734@holbertonstudents.com>
2024-01-01 19:46:07 +1100

    Fix: Revert changes

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -17,9 +17,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Returns the list of all the delays (float values)
     """
-    delays = []
+    delays: List[float] = []
     for i in range(n):
-        deley = await wait_random(max_delay)
-        delays.append(deley)
-    return_delay = sorted(delays)
-    return return_delay
+        delays.append(await wait_random(max_delay))
+    return sorted(delays)
