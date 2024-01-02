9b2b91ac6e094606ec5aac3401ecd80064213462
hewsontrinh526 <6734@holbertonstudents.com>
2024-01-02 14:31:20 +1100

    Fix: Changed Asyncgenerator to Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ A coroutine called async_generator that takes no arguments
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Yields a random number between 0 and 10
     """
