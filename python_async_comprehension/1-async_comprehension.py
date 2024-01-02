513fe855c6ea2ba60871d15423e00dc75947148e
hewsontrinh526 <6734@holbertonstudents.com>
2024-01-02 14:19:35 +1100

    Fix: Moved import statements

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,14 +3,13 @@
 A coroutine called async_comprehension that takes no arguments
 """
 import asyncio
-import random
-from typing import Generator, List
-
-async_generator = __import__('0-async_generator').async_generator
+from typing import List
 
 
 async def async_comprehension() -> List[float]:
     """
     Returns the 10 random numbers
     """
+    async_generator = __import__('0-async_generator').async_generator
+
     return [i async for i in async_generator()]
