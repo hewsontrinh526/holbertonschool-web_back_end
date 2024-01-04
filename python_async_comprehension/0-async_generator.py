92d8a9a6f1e3dc631fc179919f4dd6543a31813e
hewsontrinh526 <6734@holbertonstudents.com>
2024-01-04 16:03:38 +1100

    Feat: Improved code

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,9 @@
 """
 A coroutine called async_generator that takes no arguments
 """
-from random import random
+import random
 import asyncio
-from typing import Generator, AsyncGenerator
+from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
@@ -12,5 +12,6 @@ async def async_generator() -> Generator[float, None, None]:
     Yields a random number between 0 and 10
     """
     for i in range(10):
-        yield random() * 10
         await asyncio.sleep(1)
+        number = random.uniform(0, 10)
+        yield number
