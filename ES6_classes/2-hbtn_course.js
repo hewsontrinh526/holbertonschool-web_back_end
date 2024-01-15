c87d9142303a5b4f66cf1da53cc09e86dd697749
hewsontrinh526 <6734@holbertonstudents.com>
2024-01-16 08:43:02 +1100

    Fix: Updated type checks

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,5 +1,18 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (
+      !Array.isArray(students)
+      || !students.every((student) => typeof student === 'string')
+    ) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
     this._name = name;
     this._length = length;
     this._students = students;
@@ -29,8 +42,8 @@ export default class HolbertonCourse {
 
   set students(newStudents) {
     if (
-      !Array.isArray(newStudents)
-      || !newStudents.every((student) => typeof student === 'string')
+      !Array.isArray(newStudents) ||
+      !newStudents.every((student) => typeof student === 'string')
     ) {
       throw new TypeError('Students must be an array');
     }
