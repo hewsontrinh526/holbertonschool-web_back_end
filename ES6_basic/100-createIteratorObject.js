249b021c6c3e136574bfdbeac3eceabc61a7bf33
hewsontrinh526 <6734@holbertonstudents.com>
2024-01-16 11:03:15 +1100

    Feat: A function that will take into argument a report Object and return an iterator to go through every employee in every department

---
+++ ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+export default function createIteratorObject(report) {
+  const allEmployees = [];
+  for (const department of Object.values(report.allEmployees)) {
+    for (const employee of department) {
+      allEmployees.push(employee);
+    }
+  }
+  return allEmployees;
+}
