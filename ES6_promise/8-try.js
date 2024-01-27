d5b8a1befc0c0333d354f4ccf9f1aa3534cc63fb
hewsontrinh526 <6734@holbertonstudents.com>
2024-01-27 23:35:04 +1100

    Fix: Added try/catch

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,6 +1,11 @@
 export default function divideFunction(numerator, demoninator) {
   if (demoninator === 0) {
-    throw Error('Cannot divide by 0');
+    throw new Error('Cannot divide by 0');
   }
-  return numerator / demoninator;
+
+  try {
+    return numerator / demoninator;
+  } catch (error) {
+    throw new Error('Cannot divide by 0');
+  }
 }
