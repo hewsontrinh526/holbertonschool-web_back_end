716d41ef87e0f6e8e4b336cf2630a6cb94756996
hewsontrinh526 <6734@holbertonstudents.com>
2024-01-27 23:39:06 +1100

    Fix; Revert changes

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,12 +1,6 @@
 export default function divideFunction(numerator, demoninator) {
   if (demoninator === 0) {
-    throw new Error('Cannot divide by 0');
+    throw Error('Cannot divide by 0');
   }
-
-  try {
-    return numerator / demoninator;
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
+  return numerator / demoninator;
 }
